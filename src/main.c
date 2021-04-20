bad08c08e8e749cde17afbdd7cfe0be17cde089a
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-21 03:03:51 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
+#include <SDL2/SDL.h>
 int main(int argc , char** argv)
 {
-    printf("Hello World!");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow(
+        "Chip8 Window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,320,
+        SDL_WINDOW_SHOWN
+    );
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_TEXTUREACCESS_TARGET);
+ 
+    
+    SDL_DestroyWindow(window);
     return 0;
 }
