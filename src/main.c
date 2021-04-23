f402a7c725d5684289b6ad196295a8a5226beb28
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-23 23:14:15 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
     // chip8_keyboard_up(&chip8.keyboard,0x0f);
     // bool is_down = chip8_keyboard_is_down(&chip8.keyboard,0x0f);
     //printf("%i\n",(int)is_down);
+    chip8_init(&chip8);
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
         EMULATOR_WINDOW_TITLE,
