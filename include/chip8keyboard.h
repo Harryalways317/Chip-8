d1a0025c01f0578a00be93ac4fbd72d67dd35795
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-23 18:56:56 +0530

    Update chip8keyboard.h

--- include/chip8keyboard.h
+++ include/chip8keyboard.h
@@ -7,6 +7,9 @@ struct chip8_keyboard
     bool keyboard[CHIP8_TOTAL_KEYS];
 };
 
-
+int chip8_keyboard_map(const char* map,char key);
+void chip8_keyboard_down(struct chip8_keyboard* keyboard,int key);
+void chip8_keyboard_up(struct chip8_keyboard* keyboard,int key);
+bool chip8_keyboard_is_down(struct chip8_keyboard* keyboard,int key);
 
 #endif
