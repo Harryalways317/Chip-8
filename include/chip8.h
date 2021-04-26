7ca88e7342200718f9ffdd9380795ba492c86a9a
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-26 23:31:06 +0530

    Update chip8.h

--- include/chip8.h
+++ include/chip8.h
@@ -16,4 +16,6 @@ struct chip8
     struct chip8_screen screen;
 };
 void chip8_init(struct chip8* chip8);
+void chip8_exec(struct chip8* chip8,unsigned short opcode);
+//void chip8_load(struct chip8* chip8,const char* buf,size_t size);
 #endif
