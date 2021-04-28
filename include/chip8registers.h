9f72ea8a1d19ede96eb8e58184d98cb73288977a
Harish Vadaparty <harishvadaparty@gmail.com>
2021-04-29 01:25:01 +0530

    Update chip8registers.h

--- include/chip8registers.h
+++ include/chip8registers.h
@@ -1,5 +1,6 @@
 #ifndef CHIP8REGISTERS_H
 #define CHIP8REGISTERS_H
+
 #include "config.h"
 struct chip8_registers
 {
@@ -8,7 +9,7 @@ struct chip8_registers
     unsigned char delay_timer;
     unsigned char sound_timer;
     unsigned short PC;
-    unsigned short SP;
+    unsigned char SP;
 };
 
 #endif
